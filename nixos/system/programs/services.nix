0935ce551c8dfe2521d4f1ab479384c4ff26e745
LFour <63118374+LFour-CN@users.noreply.github.com>
2025-07-19 22:24:26 -0400

    Update services.nix

--- nixos/system/programs/services.nix
+++ nixos/system/programs/services.nix
@@ -4,44 +4,47 @@
   # Profiling (with sysprof) 
   services.sysprof.enable = true;
 
+  # Flatpak
+  services.flatpak.enable = true;
+
   # Enable Hotspot
-  services.create_ap = {
-    enable = true;
-    settings = {
-      INTERNET_IFACE = "enp4s0";
-      WIFI_IFACE = "wlan0";
-      SSID = "NixOS-Linux-6.15.2";
-      PASSPHRASE = "xbwic67&*$";
-    };
-  };
+  #services.create_ap = {
+    #enable = true;
+    #settings = {
+      #INTERNET_IFACE = "enp4s0";
+      #WIFI_IFACE = "wlan0";
+      #SSID = "NixOS-Linux-6.15.2";
+      #PASSPHRASE = "xbwic67&*$";
+    #};
+  #};
 
   # Systemd services
-  systemd.services.wpa_supplicant.serviceConfig.TimeoutSec = "2";
-  systemd.services.NetworkManager.serviceConfig.TimeoutSec = "2";
-  systemd.services.journald.serviceConfig.TimeoutSec = "2";
+  #systemd.services.wpa_supplicant.serviceConfig.TimeoutSec = "2";
+  #systemd.services.NetworkManager.serviceConfig.TimeoutSec = "2";
+  #systemd.services.journald.serviceConfig.TimeoutSec = "2";
   #systemd.services."udev-settle".serviceConfig.TimeoutSec = "2";
-  services.journald = {
-    storage = "persistent";
-    rateLimitBurst = 10000;
-    upload = {
-    settings = {
-    Upload = {
-      NetworkTimeoutSec = "3s";
-        };
-      };
-    };
-    extraConfig = ''
-      Storage=persistent
-      SystemMaxUse=50M
-      RuntimeMaxUse=50M
-      MaxFileSec=1week
-    '';
-  };
+  #services.journald = {
+    #storage = "persistent";
+    #rateLimitBurst = 10000;
+    #upload = {
+    #settings = {
+    #Upload = {
+      #NetworkTimeoutSec = "3s";
+        #};
+      #};
+    #};
+    #extraConfig = ''
+      #Storage=persistent
+      #SystemMaxUse=50M
+      #RuntimeMaxUse=50M
+      #MaxFileSec=1week
+    #'';
+  #};
 
   services.mpd = {
     enable = true;
     user = "nixos"; 
-    musicDirectory = "/home/nixos/Music";
+    musicDirectory = "/home/lfour/Music";
     # Optional:
     network.listenAddress = "any"; # if you want to allow non-localhost connections
     startWhenNeeded = true; # systemd feature: only start MPD service upon connection to its socket
@@ -62,8 +65,8 @@
     XDG_RUNTIME_DIR = "/run/user/1000";
   };
 
-  services.emacs.enable = true;
-
+  # Emacs editor
+  #services.emacs.enable = true;
   #services.flatpak.enable = true;
   #systemd.services.flatpak-repo = {
   #  wantedBy = [ "multi-user.target" ];
@@ -79,4 +82,3 @@
       sysprof
   ];
 }
-
